c20d7d3716e336966cdc347158ea061ece9b01f7
Asfak Rahman <asfakr877@gmail.com>
2026-08-18 20:37:37 +0530

    Remove all files from main branch

--- backend/services/recommendation/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Recommendation Services Package
-Exports Recommendation Engine and User Preference Scorers.
-"""
-from services.recommendation_service import RecommendationService
-
-__all__ = [
-    "RecommendationService",
-]
